20669c02f7b641cfec2c41dcba3b4de86353477c
Minh Nguyen <minh.d.nguyen2211@gmail.com>
2016-09-29 10:27:47 +0000

    Consider an array of  integers, where all but one of the integers occur in pairs. In other words, every element in  occurs exactly twice except for one unique element - cracking coding interview

---
+++ lonelyInteger.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        System.out.println(lonelyInteger(a));
+    }
+    
+    private static int lonelyInteger (int[] A) {
+        int num = 0;
+        for (int a : A) {
+            num = num ^ a;
+        }
+        return num;
+    }
+}
