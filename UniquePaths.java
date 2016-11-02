06b285a8a991a06d09bfbc105e6c8f61b1d3ae41
Minh Nguyen <minh.d.nguyen2211@gmail.com>
2016-11-02 10:57:56 +0000

    UniquePaths problem - leetcode

---
+++ UniquePaths.java
@@ -0,0 +1,27 @@
+public class Solution {
+    
+    private HashMap<ArrayList<Integer>, Integer> map = new HashMap<ArrayList<Integer>, Integer>();
+    
+    public int uniquePaths(int m, int n) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(m);
+        list.add(n);
+        
+        if (map.containsKey(list))
+            return map.get(list);
+        if (m == 1 && n == 1)
+            return 1;
+        
+        int noOfPathsRight = 0;
+        int noOfPathsDown = 0;
+        if (n > 1)
+            noOfPathsRight = uniquePaths(m, n-1);
+        if (m > 1)
+            noOfPathsDown = uniquePaths(m - 1, n);
+            
+        int noOfPaths = noOfPathsRight + noOfPathsDown;
+        map.put(list, noOfPaths);
+        return noOfPaths;
+    }
+}
