38b2d8445ed12ca3beb1058edea0db66a6522989
Minh Nguyen <minh.d.nguyen2211@gmail.com>
2017-03-13 13:17:40 +1000

    Update countSay.java

--- countSay.java
+++ countSay.java
@@ -2,25 +2,21 @@ public class Solution {
     public String countAndSay(int n) {
         String res = "1";
         for (int i = 2; i <= n; i++) {
-            res = generateSeq(res);
-        }
-        return res;
-    }
-    
-    public String generateSeq (String s) {
-        char cursor = s.charAt(0);
-        int count = 1;
-        String res = "";
-        for (int i = 1; i < s.length(); i++) {
-            if (cursor != s.charAt(i)) {
-                res += String.valueOf(count) + Character.toString(cursor);
-                cursor = s.charAt(i);
-                count = 1;
-                continue;
+            int count = 0;
+            char marker = res.charAt(0);
+            String temp = "";
+            for (char c : res.toCharArray()) {
+                if (marker == c) {
+                    count++;
+                } else {
+                    temp += Integer.toString(count) + Character.toString(marker);
+                    count = 1;
+                }
+                marker = c;
             }
-            count++;
+            temp += Integer.toString(count) + Character.toString(marker);
+            res = temp;
         }
-        res += String.valueOf(count) + Character.toString(cursor);
         return res;
     }
 }
