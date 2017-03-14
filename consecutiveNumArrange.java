a1fa75dc7cc110daf8ec7cfabc15dfdf6e904a3d
Minh Nguyen <minh.d.nguyen2211@gmail.com>
2017-03-14 15:31:34 +1000

    Update consecutiveNumArrange.java

--- consecutiveNumArrange.java
+++ consecutiveNumArrange.java
@@ -6,9 +6,14 @@ public int arrange(int[] A) {
     for (int i = 0; i < A.length; i++) {
         int windowStart = A[i];
         int windowEnd = windowStart + balls - 1;
+        if (windowEnd > 1000000000)
+            break;
+        
         int endIndexOfAInWindow = Arrays.binarySearch(A, windowEnd);
+        
         if (endIndexOfAInWindow < 0)
             endIndexOfAInWindow = endIndexOfAInWindow * (-1) - 1 - 1;
+        
         int ballsInWindow = endIndexOfAInWindow - i + 1;
         if (ballsInWindow > max)
             max = ballsInWindow;
