c741b1ac350c8a7273251eba3b4f74c166bf9fee
Minh Nguyen <minh.d.nguyen2211@gmail.com>
2017-03-14 15:11:40 +1000

    Create consecutiveNumArrange.java
    https://www.codechef.com/RUBIX10/problems/RUBIX4

---
+++ consecutiveNumArrange.java
@@ -0,0 +1,18 @@
+public int arrange(int[] A) {
+    Arrays.sort(A);
+    int balls = A.length;
+    int max = -1;
+
+    for (int i = 0; i < A.length; i++) {
+        int windowStart = A[i];
+        int windowEnd = windowStart + balls - 1;
+        int endIndexOfAInWindow = Arrays.binarySearch(A, windowEnd);
+        if (endIndexOfAInWindow < 0)
+            endIndexOfAInWindow = endIndexOfAInWindow * (-1) - 1 - 1;
+        int ballsInWindow = endIndexOfAInWindow - i + 1;
+        if (ballsInWindow > max)
+            max = ballsInWindow;
+    }
+
+    return balls - max;
+}
